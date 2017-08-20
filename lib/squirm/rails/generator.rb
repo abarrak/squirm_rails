700f91fc8aa50269e17912824b0939bb262e3b30
Abdullah Barrak <abdullah@abarrak.com>
2017-08-20 10:46:50 +0300

    update test generator directory to /db

--- lib/squirm/rails/generator.rb
+++ lib/squirm/rails/generator.rb
@@ -5,7 +5,7 @@ module Squirm
     source_root File.dirname(__FILE__)
     def install_functions
       copy_file "functions.sql", "db/functions.sql"
-      copy_file "unit_test.rb", "test/unit/stored_procedures/hello_world_test.rb"
+      copy_file "unit_test.rb", "test/db/stored_procedures/hello_world_test.rb"
     end
   end
 end
